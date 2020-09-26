a53c21930e2d7cd06abaa38aa2a0074b26307433
Melih Kaan <melihkaanozkan@icloud.com>
2020-09-26 12:36:44 +0300

    Initial Commit

---
+++ MovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by Melih Özkan on 26.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Melih Özkan on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
